386cd5b3f6f170d15f1eee298da4cdef164e47f8
gopalbala <balasubramaniangopalakrishnan@gmail.com>
2020-06-08 00:54:08 +0530

    Changes during recording.

--- src/main/java/com/gb/leaderelection/common/LeaderElectionConstants.java
+++ src/main/java/com/gb/leaderelection/common/LeaderElectionConstants.java
@@ -3,5 +3,5 @@ package com.gb.leaderelection.common;
 public class LeaderElectionConstants {
     public static final String ZOOKEEPER_ADDRESS = "localhost:2181";
     public static final int SESSION_TIMEOUT = 3000;
-    public static final String ELECTION_NAMESPACE = "/election";
+    public static final String ELECTION_NAMESPACE = "/leader-election";
 }
