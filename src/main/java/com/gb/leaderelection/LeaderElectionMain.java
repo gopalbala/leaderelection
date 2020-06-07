5153f213fd838ce334c1297a8709ba32cf9ddc3b
gopalbala <balasubramaniangopalakrishnan@gmail.com>
2020-06-07 19:03:00 +0530

    Driver code.

--- src/main/java/com/gb/leaderelection/LeaderElectionMain.java
+++ src/main/java/com/gb/leaderelection/LeaderElectionMain.java
@@ -1,7 +1,19 @@
 package com.gb.leaderelection;
 
-public class LeaderElectionMain {
-    public static void main(String[] args) {
+import com.gb.leaderelection.keeper.LeaderElection;
+import org.apache.zookeeper.KeeperException;
 
+import java.io.IOException;
+
+public class LeaderElectionMain {
+    public static void main(String[] args) throws IOException, KeeperException, InterruptedException {
+        LeaderElection leaderElection = new LeaderElection();
+        leaderElection.connect();
+        leaderElection.volunteerForLeadership();
+        leaderElection.electLeader();
+        leaderElection.run();
+        leaderElection.close();
+
+        System.out.println("I am done for the day.... Connect with you some other time... :)");
     }
 }
